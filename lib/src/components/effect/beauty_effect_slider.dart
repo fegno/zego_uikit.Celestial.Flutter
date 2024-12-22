713840c86fad1a3decda95242531984b589002fd
yuyanjun <yuyanjun@zego.im>
2024-12-22 09:39:06 +0800

    add ZegoBeautyEffectSlider

--- lib/src/components/effect/beauty_effect_slider.dart
+++ lib/src/components/effect/beauty_effect_slider.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 // Project imports:
 import 'package:zego_uikit/src/components/screen_util/screen_util.dart';
+import 'package:zego_uikit/src/components/widgets/slider.dart';
 import 'package:zego_uikit/src/services/services.dart';
 
 /// @nodoc
@@ -53,62 +54,22 @@ class ZegoBeautyEffectSlider extends StatefulWidget {
 
 /// @nodoc
 class _ZegoBeautyEffectSliderState extends State<ZegoBeautyEffectSlider> {
-  var valueNotifier = ValueNotifier<int>(50);
-
-  @override
-  void initState() {
-    super.initState();
-
-    valueNotifier.value = widget.defaultValue;
-  }
-
   @override
   Widget build(BuildContext context) {
-    valueNotifier.value = widget.defaultValue;
-
-    final thumpHeight = widget.thumpHeight ?? 32.zH;
-    return SizedBox(
-      width: 480.zW,
-      height: thumpHeight,
-      child: SliderTheme(
-        data: SliderTheme.of(context).copyWith(
-          valueIndicatorTextStyle: widget.textStyle ??
-              TextStyle(
-                color: const Color(0xff1B1A1C),
-                fontSize: 30.zR,
-                fontWeight: FontWeight.w400,
-              ),
-          valueIndicatorColor:
-              widget.textBackgroundColor ?? Colors.white.withOpacity(0.5),
-          activeTrackColor: widget.activeTrackColor ?? Colors.white,
-          inactiveTrackColor: widget.inactiveTrackColor ??
-              const Color(0xff000000).withOpacity(0.3),
-          trackHeight: widget.trackHeight ?? 6.0.zR,
-          thumbColor: widget.thumbColor ?? Colors.white,
-          thumbShape: RoundSliderThumbShape(
-              enabledThumbRadius: widget.thumbRadius ?? thumpHeight / 2.0),
-        ),
-        child: ValueListenableBuilder<int>(
-          valueListenable: valueNotifier,
-          builder: (context, value, _) {
-            return Slider(
-              value: value.toDouble(),
-              min: 0,
-              max: 100,
-              divisions: 100,
-              label: widget.labelFormatFunc == null
-                  ? value.toDouble().round().toString()
-                  : widget.labelFormatFunc?.call(value),
-              onChanged: (double defaultValue) {
-                valueNotifier.value = defaultValue.toInt();
-
-                ZegoUIKit()
-                    .setBeautifyValue(defaultValue.toInt(), widget.effectType);
-              },
-            );
-          },
-        ),
-      ),
+    return ZegoSlider(
+      defaultValue: widget.defaultValue,
+      onChanged: (double value) {
+        ZegoUIKit().setBeautifyValue(value.toInt(), widget.effectType);
+      },
+      thumpHeight: widget.thumpHeight,
+      trackHeight: widget.trackHeight,
+      labelFormatFunc: widget.labelFormatFunc,
+      textStyle: widget.textStyle,
+      textBackgroundColor: widget.textBackgroundColor,
+      activeTrackColor: widget.activeTrackColor,
+      inactiveTrackColor: widget.inactiveTrackColor,
+      thumbColor: widget.thumbColor,
+      thumbRadius: widget.thumbRadius,
     );
   }
 }
